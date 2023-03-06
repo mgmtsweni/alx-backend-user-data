caa6908fdba3ed6a63934c40e34517416f18b087
mgmtsweni <mtswenigift@gmail.com>
2023-03-06 23:25:53 +0200

    filter log

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Base 64 encryption"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ A func that Use the bcrypt package to perform the hashing"""
+    codedPass = password.encode()
+    return bcrypt.hashpw(codedPass, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ A function that Use bcrypt to validate that the provided
+        password matches the hashed password.
+    """
+    valid = False
+    pass_encoded = password.encode()
+    if bcrypt.checkpw(pass_encoded, hashed_password):
+        valid = True
+    return valid
