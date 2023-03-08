1a0ecd0009c941c0b9e940f078bae347c753a80a
mgmtsweni <mtswenigift@gmail.com>
2023-03-08 23:06:46 +0200

    update

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,6 +4,7 @@ import os
 from os import getenv
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
@@ -22,13 +23,13 @@ elif Auth == "basic_auth":
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler"""
+    """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler."""
+    """Unauthorized handler."""
     return jsonify({"error": "Unauthorized"}), 401
 
 @app.errorhandler(403)
@@ -39,8 +40,7 @@ def forbidden(error) -> str:
 
 @app.before_request
 def before_request() -> str:
-    """ Filter each request
-    """
+    """ Filter each request"""
     path_list = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
@@ -54,6 +54,7 @@ def before_request() -> str:
 
 
 if __name__ == "__main__":
+    """Main Func"""
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
